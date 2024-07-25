8f2663afed5f687d46730102df300e5471fec5af
Varun0726 <virinders026@gmail.com>
2024-07-25 17:48:49 -0400

    made changes to the get and post request

--- Express/Hospital.js
+++ Express/Hospital.js
@@ -1,17 +1,42 @@
 const express = require("express")
 const app = express()
 const port = 3000;
-let healthyKidney = 2;
-let unhealthyKidney = 0;
+var User = [{
+    name: "John",
+    Kidneys:[{number:1,
+        health:"healthy"
+    },{
+        number:2,
+        health:"Unhealth"
+    }]
+}]
+function numberKidney(x){
+    let kidney = 0;
+    if (x.health == "healthy"){
+        kidney++
+    }
+    return kidney;
+}
 
 app.get("/",(req,res) =>{
-    res.send(`healthy kidney = ${healthyKidney},unhealthy kidney = ${unhealthyKidney}`)
+    const numberofKidney = User[0].Kidneys.length;
+    const healthKidneys = User[0].Kidneys.filter(numberKidney).length
+    res.json({
+        numberofKidney,
+        healthKidneys
+    });
 })
 
 app.post("/",(req,res) =>{
-    unhealthyKidney += 1;
-    healthyKidney -= 1;
-    res.send(`healthy kidney = ${healthyKidney},unhealthy kidney = ${unhealthyKidney}`)
+    const numberofKidney = User[0].Kidneys.length;
+    const newKidney = {
+        number : numberofKidney,
+        health : "healthy"
+    }
+    User[0].Kidneys.push(newKidney);
+    res.json({
+        msg:"Done"
+    })
 })
 
 app.put("/",(req,res) =>{
