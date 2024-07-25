411179ccfd430d7bc4368f007d625ed9d17557ec
Varun0726 <virinders026@gmail.com>
2024-07-25 16:55:57 -0400

    bug fixes

--- basic-1.js
+++ basic-1.js
@@ -1,19 +1,19 @@
 const express = require("express");
 const app = express();
-const Port = 3000;
+const Port = 4000;
 
 function CalculateSum(n){
-    let variable = n;
+    let variable = 0;
     for (let i =0;i<=n;i++){
         variable = variable+i;
     }
     return variable;
 }
 
-app.get("/",(res,req) => {
+app.get("/",(req,res) => {
     const n = req.query.n;
     const ans = CalculateSum(n);
-    res.setEncoding(ans);
+    res.send(ans.toString());
 })
 
 app.listen(Port);
