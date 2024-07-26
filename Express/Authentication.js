852bc679e362f8228806f24d2b43bb60b6f4a6fc
Varun0726 <virinders026@gmail.com>
2024-07-26 16:18:16 -0400

    Created the Usercheck middleware and created some more routes

--- Express/Authentication.js
+++ Express/Authentication.js
@@ -4,17 +4,31 @@ const port = 3000;
 
 app.use(express.json());
 
-app.post("/:username",(req,res) =>{
-    const username = req.params.username;
-    const password = req.body.password;
+function UserCheck(req,res,next){
+    const username = req.headers.username;
+    const password = req.headers.password;
     if (username != "Varun" || password != "1234"){
         res.status(403).json({
             msg:"Sorry you enter either wrong password or username"
         })
+    }else{
+        next()
     }
-    res.send({
-        msg:"Welcome to the Application!"
-    })
+}
+
+
+
+app.get("/",UserCheck,(req,res) =>{
+   //
+})
+
+
+app.post("/Add",UserCheck,(req,res) =>{
+    //
+})
+
+app.put("/update",UserCheck,(req,res) => {
+    //
 })
 
 
